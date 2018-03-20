3e8303034b74fe9aa1ea41f3f99aa8430de1bc81
icew4ll <icew4ll.io>
2018-03-20 09:01:24 -0700

    vec csv

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
-//tutorial-setup-01.rs
-// This makes the csv crate accessible to your program.
 extern crate csv;
+// extern crate ssh2;
 
-// Import the standard library's I/O module so we can read from stdin.
-use std::io;
+// use std::net::TcpStream;
+// use ssh2::Session;
+use std::error::Error;
+use std::fs::File;
+use std::process;
 
-// The `main` function is where your program starts executing.
-fn main() {
-    // Create a CSV parser that reads data from stdin.
-    let mut rdr = csv::Reader::from_reader(io::stdin());
-    // Loop over each record.
-    for result in rdr.records() {
-        // An error may occur, so abort the program in an unfriendly way.
-        // We will make this more friendly later!
-        let record = result.expect("a CSV record");
-        // Print a debug version of the record.
-        println!("{:?}", record);
+type Record = (String, String, String);
+
+fn run(conn: &mut Vec<Record>) -> Result<(), Box<Error>> {
+    let file = File::open("conn.csv")?;
+    let mut rdr = csv::ReaderBuilder::new().flexible(true).from_reader(file);
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        // println!("{:?}", record);
+        conn.push(record);
     }
+    Ok(())
+}
+
+fn main() {
+    let mut conn = Vec::new();
+    // let mut out = Vec::new();
+    if let Err(err) = run(&mut conn) {
+        println!("{}", err);
+        process::exit(1);
+    }
+
+    println!("{:?}", conn);
+    println!("{:?}", conn[0].1);
 }
